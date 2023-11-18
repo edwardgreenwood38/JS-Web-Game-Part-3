d6e8f773054b7d8ae2c6d16cfecb132f76e3c18f
Edward E Greenwood <edwardgreenwood38@gmail.com>
2023-11-18 17:41:14 -0500

    step 4 completed

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ function newImage(url) {
 }
 
 //newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
+//newImage('assets/tree.png', 200, 450)
+//newImage('assets/pillar.png', 350, 250)
+//newImage('assets/pine-tree.png', 450, 350)
+//newImage('assets/crate.png', 150, 350)
+//newImage('assets/well.png', 500, 575)
 
 
-function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
+function newItem(url){
+    let item = newImage(url)
     item.addEventListener('click', () => {
         item.remove()
         let inventoryItem = document.createElement('img')
@@ -24,9 +24,7 @@ function newItem(url, left, bottom){
     return item
 }
 
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
+
 
 function newInventory(){
     let inventory = document.createElement('div')
@@ -61,11 +59,16 @@ function move(image) {
     }
 }
 
-// let greenCharacter = newImage('assets/green-character.gif')
-// let thingThatMoveReturns = move(greenCharacter)
-// thingThatMoveReturns.to(100, 250)
-
-// let greenCharacter = newImage('assets/green-character.gif')
-// move(greenCharacter).to(100, 250)
 
 move(newImage('assets/green-character.gif')).to(200, 250)
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
+
+
